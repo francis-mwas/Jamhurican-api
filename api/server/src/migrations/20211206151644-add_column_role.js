ae111ded423e997248e527f519ee413bbe15e4de
francis-mwas <mwangif184@gmail.com>
2021-12-09 22:49:58 +0300

    Added migrations

---
+++ api/server/src/migrations/20211206151644-add_column_role.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('users', 'role', {
+      type: Sequelize.ENUM('normal-user', 'admin'),
+      defaultValue: 'normal-user',
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('users', 'role');
+  },
+};
