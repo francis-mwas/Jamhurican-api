b48093f8c761e3c94726eceeac4b18a4ff389ec4
francis-mwas <mwangif184@gmail.com>
2022-02-26 19:29:04 +0300

    Added package

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,9 @@ module.exports = {
     '@babel/preset-env',
     '@babel/plugin-syntax-jsx',
   ],
-  plugins: ['@babel/transform-runtime', '@babel/plugin-syntax-jsx'],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/transform-runtime',
+    '@babel/plugin-syntax-jsx',
+  ],
 };
