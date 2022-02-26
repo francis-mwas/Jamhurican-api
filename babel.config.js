4ae187b85488c528f3dd63a67a1b81d72d78a2a1
francis-mwas <mwangif184@gmail.com>
2022-02-26 18:57:38 +0300

    modified babel file

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/plugin-syntax-jsx',
+  ],
+  plugins: [
+    '@babel/transform-runtime',
+    '@babel/plugin-syntax-jsx',
+    'jsx',
+    'flow',
+    'typescript',
+  ],
 };
