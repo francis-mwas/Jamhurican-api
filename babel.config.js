79e50e0375fdeacf0dc35e958d11ab06881156b4
francis-mwas <mwangif184@gmail.com>
2022-02-26 19:24:51 +0300

    trying to fix babel issue on heroku

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,8 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
     '@babel/preset-react',
+    '@babel/preset-env',
     '@babel/plugin-syntax-jsx',
   ],
-  plugins: [
-    '@babel/transform-runtime',
-    '@babel/plugin-syntax-jsx',
-    'jsx',
-    'flow',
-    'typescript',
-  ],
+  plugins: ['@babel/transform-runtime', '@babel/plugin-syntax-jsx'],
 };
