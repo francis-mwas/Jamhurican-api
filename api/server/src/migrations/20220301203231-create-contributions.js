cf6cb00be8304eff2beeea218b764127bf50a135
francis-mwas <mwangif184@gmail.com>
2022-03-02 00:22:57 +0300

    changed contribution migrations id field

--- api/server/src/migrations/20220301203231-create-contributions.js
+++ api/server/src/migrations/20220301203231-create-contributions.js
@@ -6,7 +6,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
       },
       amount: {
         type: Sequelize.INTEGER,
