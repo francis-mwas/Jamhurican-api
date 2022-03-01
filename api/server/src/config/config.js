985953172e087917b55b84b223c2c32f25f5a46b
francis-mwas <mwangif184@gmail.com>
2022-03-02 00:09:39 +0300

    Changed user model id data type

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -23,15 +23,15 @@ module.exports = {
     dialect: 'postgres',
   },
 
-  // production: {
-  //   database: process.env.DB_NAME,
-  //   username: process.env.DB_USER,
-  //   password: process.env.DB_PASS,
-  //   host: process.env.DB_HOST,
-  //   port: process.env.DB_PORT,
-  //   dialect: 'postgres',
-  //   ssl: true,
-  // },
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    ssl: true,
+  },
   dialectOptions: {
     ssl: true,
   },
